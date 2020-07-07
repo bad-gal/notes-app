d481c305e70de3cda976c21f0b138b8b691b19e4
Andrien Ricketts <knowledgehut.developments@gmail.com>
2020-07-07 21:29:32 +0100

    User can create notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,4 +6,29 @@ class NotesController < ApplicationController
   def index
     @notes = Note.where(user_id: current_user.id)
   end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(notes_params)
+
+    if @note.save
+      flash[:notice] = t('notes.create.success')
+      redirect_to note_path(@note.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @note = Note.find_by(params[:id])
+  end
+
+  private
+
+  def notes_params
+    params.require(:note).permit(:user_id, :subject, :text)
+  end
 end
--- spec/requests/notes_request_spec.rb
+++ spec/requests/notes_request_spec.rb
@@ -12,4 +12,31 @@ RSpec.describe 'Notes', type: :request do
     expect(response).to render_template(:index)
     expect(response.status).to eq(200)
   end
+
+  it 'creates a note and redirects to the show page' do
+    user = create(:user)
+    login_user(user)
+
+    get new_note_path
+    expect(response).to render_template(:new)
+
+    post notes_path, params: { note: { user_id: user.id, subject: 'My first note', text: 'This is a test' } }
+
+    expect(response).to redirect_to(assigns(:note))
+    follow_redirect!
+
+    expect(response).to render_template(:show)
+    expect(response.body).to include(I18n.t('notes.create.success'))
+  end
+
+  it 'fails to create a note and returns to the new page' do
+    user = create(:user)
+    login_user(user)
+
+    get new_note_path
+    expect(response).to render_template(:new)
+
+    post notes_path, params: { note: { user_id: user.id, subject: nil, text: 'This is a test' } }
+    expect(response).to render_template(:new)
+  end
 end
