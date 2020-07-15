fba4bcaeb97efbcd256e5b07c9070abafb4e1583
Andrien Ricketts <knowledgehut.developments@gmail.com>
2020-07-15 20:36:21 +0100

    Initial skeleton of settings controller created

---
+++ spec/features/user_visits_settings_page.spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.feature 'User visits settings page' do
+  xscenario 'visiting user can not access settings page' do
+  end
+
+  xscenario 'user can change email address' do
+  end
+
+  xscenario 'user can change password' do
+  end
+
+  xscenario 'user can delete account' do
+  end
+end
